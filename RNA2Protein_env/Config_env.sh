d71ba99af869493f7b4d0ef7bcf2c4ee403d4da5
wsy1216 <94498007+wsy1216@users.noreply.github.com>
2023-04-13 11:30:09 +0800

    Updata RNA2Protein_env

---
+++ RNA2Protein_env/Config_env.sh
@@ -0,0 +1,5 @@
+conda env create -f BabelScmogcnCmae_env.yml
+conda env create -f DengkwScarchesCtpnet_env.yml
+conda env create -f R_env.yml
+conda env create -f scipenn_env.yml
+conda env create -f totalvi_env.yml
